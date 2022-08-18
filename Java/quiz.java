c48400b7d85fc88ed8430fe75ea67f48c61c2c97
averi <aashu053@learner.dvusd.org>
2022-08-18 11:39:20 -0700

    thursday

---
+++ Java/quiz.java
@@ -0,0 +1,20 @@
+
+
+
+
+public class quiz
+{
+    
+    public static void main(String args[])
+    
+    {
+   System.out.println("How many of your pets have died? A. 12 B. 4 C. 9");
+   System.out.println("What color is your underwear? A. Brown B. Orange C. Pink");
+   System.out.println("What is your mother's name? A. Susan B. Zoe C. Clarissa");
+    
+    }
+
+    
+    
+}
+
